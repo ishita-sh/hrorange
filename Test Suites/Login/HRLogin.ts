38e20acb4e2165aa7ed52ce005477ea1e8ca649f
jain.hims91 <himanshu.jain@ranosys.com>
2019-08-08 15:25:26 +0530

    first commit

---
+++ Test Suites/Login/HRLogin.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HRLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6af29e59-19e5-466e-a6e0-5514ace72da7</testSuiteGuid>
+   <testCaseLink>
+      <guid>a00363b4-d409-476c-bb02-46a726c174da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_login/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
